c5ce8b7448c37e4b0e9e428e245cc9401ef5e761
suhyunbaik <suhyunbaik89@gmail.com>
2023-06-09 12:13:22 +0900

    Stop using custom loader

--- next.config.js
+++ next.config.js
@@ -4,16 +4,18 @@ const withNextra = require('nextra')({
     theme: 'nextra-theme-docs',
     themeConfig: './theme.config.jsx'
 })
+// annotated for temp
+// module.exports = withNextra({
+//     images: {
+//         loader: 'custom',
+//         unoptimized: true,
+//         disableStaticImages: true
+//     },
+//     reactStrictMode: true,
+//     trailingSlash: true
+// })
 
-module.exports = withNextra({
-    images: {
-        loader: 'custom',
-        unoptimized: true,
-        disableStaticImages: true
-    },
-    reactStrictMode: true,
-    trailingSlash: true
-})
+module.exports = withNextra()
 
 // If you have other Next.js configurations, you can pass them as the parameter:
 // module.exports = withNextra({ /* other next.js config */ })
